a648086911c314700ff64f9bf65ec9f590e652d3
LCgaga <402897564@qq.com>
2016-12-26 15:56:22 +0800

    all completed

--- app/src/main/java/com/example/administrator/myminiweather/miniWeather/MainActivity.java
+++ app/src/main/java/com/example/administrator/myminiweather/miniWeather/MainActivity.java
@@ -394,13 +394,13 @@ public class MainActivity extends Activity implements OnClickListener {
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
         // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
-        oks.setTitle("标题");
+        oks.setTitle("MiniWeather分享");
         // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
         oks.setTitleUrl("http://sharesdk.cn");
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText(city_name_Tv.getText()+" "+climateTv.getText()+" 温度："+temperature_now.getText());
         //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
-        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        oks.setImageUrl("http://www.easyicon.net/api/resizeApi.php?id=1174131&size=128");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
